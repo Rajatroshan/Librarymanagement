eea8914805b09471b6e23a96aa85d02edba8de1c
CHAMOD <malinthachamod139@gmail.com>
2018-11-11 15:27:50 +0530

    first commit

---
+++ src/JavaConnection.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author CM139
+ */
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class JavaConnection {
+    Connection conn;
+    
+    public static Connection ConnectDb(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn=DriverManager.getConnection("jdbc.sqlite");
+            return conn;
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null,e);
+            return null;
+        }
+    }
+    
+}
